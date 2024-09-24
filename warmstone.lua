7a1e6de86fe0253a9e8561f38993128af063a077
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-09-24 00:40:44 -0500

    Add files via upload

--- warmstone.lua
+++ warmstone.lua
@@ -9,7 +9,7 @@ minetest.register_node(modname .. ":stone_hot", {
 	description = "Stone",
 	tiles = {"(nc_terrain_stone.png^[colorize:red:32)^(nc_terrain_stone_hard.png^[opacity:64)"},
 		groups = {
---			stone = 1,
+			stone_hot = 1,
 --			rock = 1,
 --			smoothstone = 1,
 			cracky = 2
@@ -22,3 +22,14 @@ minetest.register_node(modname .. ":stone_hot", {
 		mapcolor = {r = 75, g = 70, b = 70}
 })
 ------------------------------------------------------------------------
+minetest.register_abm({
+		label = "warmstone cooling",
+		interval = 60
+		chance = 20
+		nodenames = {modname .. ":stone_hot"},
+		action = function(pos)
+			if nodecore.find_nodes_around(pos, "group:lava", 3) then return end
+			return minetest.set_node(pos, {name = "nc_terrain:hard_stone_1"})
+		end
+	})
+
